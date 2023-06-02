e46133fb27361575cd61b10bb5ac4f1aff5fcd66
nicolas thieblemont <xxtraide@gmail.com>
2023-06-02 18:55:55 +0200

    tes

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/css/all.css">
+    <title>Nicolas Thieblemont - Portfolio </title>
+</head>
+
+<body>
+
+    <header>
+        <nav id="nav-container">
+            <img src="assets/img/logo_b.png" alt="logo du site" id="logo">
+            <div id="nav-text">
+                <p class="nav-item">A propos</p>
+                <p class="nav-item">Competence</p>
+                <p class="nav-item">Projet</p>
+                <p class="nav-item">Contact</p>
+            </div>
+            <div id="nav-link">
+                <a href="https://github.com/xtraide"><img class="logo-link" src="assets/img/github banc.png" alt="logo git"></a>
+                <a href="https://www.linkedin.com/in/nicolas-thieblemont-73b25a209/"><img class="logo-link" src="assets/img/linkedinlogoB.png" alt="logo linkedin"></a>
+            </div>
+        </nav>
+    </header>
+    <div id="ac-container">
+        <h1 id="me">Je suis<br>
+            Nicolas Thieblemont
+            <br>developpeur back end
+        </h1>
+        <div id="ac-button">
+            Passer a la suite
+        </div>
+        <hr id="ac-hr">
+    </div>
+
+    <div id="about-container">
+        <h2>A propos</h2>
+        <img src="assets/img/IMG_2955.jpg" alt="sefl picture" id="about-img">
+        <p id="about-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo doloribus molestias non sit fuga architecto, quam vel obcaecati nisi consectetur temporibus dolor quasi quisquam expedita voluptatum. Officia at praesentium delectus!
+        </p>
+        <hr>
+    </div>
+
+</body>
+
+
+</html>
